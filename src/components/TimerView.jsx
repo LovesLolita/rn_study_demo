d3e3acefe345b32cff2bb7e2f7f0a20f5b575586
LoveLolita <q781934731@gmail.com>
2023-05-14 20:27:13 +0800

    add images

--- src/components/TimerView.jsx
+++ src/components/TimerView.jsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
+import girlImg from '../assets/img/girl.jpg';
+import girlImgTwo from '../assets/img/girl2.png';
 
 const TimerView = () => {
   const [num, setNum] = useState(0);
@@ -14,26 +16,42 @@ const TimerView = () => {
     };
   }, []);
 
-
-  const viewRef = useRef(null)
-  const TimerViewLayOut = (e) => {
+  const viewRef = useRef(null);
+  const TimerViewLayOut = e => {
     console.log(e.nativeEvent);
-  }
+  };
 
   useEffect(() => {
     setTimeout(() => {
       viewRef.current.setNativeProps({
         style: {
-          backgroundColor: '#ccc'
-        }
-      })
-    }, 2000)
-  }, [])
+          backgroundColor: '#ccc',
+        },
+      });
+    }, 2000);
+  }, []);
+
 
   return (
-    <View ref={viewRef} style={styles.TimerView} onLayout={(e) => TimerViewLayOut(e)}>
+    <View
+      ref={viewRef}
+      style={styles.TimerView}
+      onLayout={e => TimerViewLayOut(e)}>
       <Text style={[styles.textMarginTop, styles.TimeText]}>计时器</Text>
       <Text style={styles.TimeText}>{num}</Text>
+      <Image
+        source={girlImgTwo}
+        style={styles.img}
+        blurRadius={0}
+        defaultSource={girlImg}
+        fadeDuration={2000}
+        onLoad={() => {
+          console.log('onLoad');
+        }}
+        onError={() => {
+          console.log('onError');
+        }}
+      />
     </View>
   );
 };
@@ -51,6 +69,14 @@ const styles = StyleSheet.create({
     color: '#1d67f7',
     fontSize: 70,
     textAlign: 'center',
+  },
+  img: {
+    margin: 50,
+    backgroundColor: '#000',
+    width: 260,
+    height: 220,
+    resizeMode: 'cover',
+    // tintColor: '#fff'
   },
 });
 
